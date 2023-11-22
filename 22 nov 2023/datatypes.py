57eb26d4f4a2255cadd1538a710145751472f4d4
manvi <manvigarg13306@gmail.com>
2023-11-22 17:03:32 +0530

    first project

---
+++ 22 nov 2023/datatypes.py
@@ -0,0 +1,40 @@
+#1. Variables:
+   # Define five different variables showcasing various data types (integers, strings, lists, tuples, dictionaries).
+   #Print each variable along with its data type.
+
+#2. Data Types:
+   #Define and explain different data types available in Python (int, float, str, list, tuple, dict).
+   #Provide an example for each data type showcasing its usage.
+
+#interger example
+a=100
+print(type(a))
+print(a)
+
+#float example
+x=10.5
+print(type(x))
+print(x)
+
+#string example
+a='manvi garg'
+print(type(a))
+print(a)
+
+#list example
+list1=[1,2,3,'manvi']
+print(type(list1))
+print(list1)
+
+#dictionary example
+dict1={1:'manvi',2:'garg'}
+print(type(dict1))
+print(dict1)
+
+#tuple example
+tup1=('12,23,34,45')
+print(type(tup1))
+print(tup1)
+
+
+
