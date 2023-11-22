298f57fa718ed3b56886eef99a4532f7c7918501
manvi <manvigarg13306@gmail.com>
2023-11-22 17:17:26 +0530

    second commit

---
+++ 22 nov 2023/conditional.py
@@ -0,0 +1,9 @@
+'''3. Conditional Statements:
+   - Implement an `if...else` statement to check whether a number is even or odd.
+   - Use the modulo operator `%` to perform this check.'''
+
+x=int(input('enter a number'))
+if x%2==0:
+    print('number is even')
+else:
+    print('number is odd')
---
+++ 22 nov 2023/function.py
@@ -0,0 +1,10 @@
+'''5. Functions:
+   - Define a function that takes two arguments and returns their sum.
+   - Demonstrate the function by calling it with different sets of arguments.'''
+
+def sumfun(x,y):
+    sum=x+y
+    return sum
+x=10
+y=20
+print(sumfun(x,y))
